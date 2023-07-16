bdd464f888c5587b641968a7c4ef5fd5ad68f653
Thurlow88 <130718052+Thurlow88@users.noreply.github.com>
2023-07-15 21:34:01 -0600

    Disable Lin Advance and decrease Junction Deviation
    Decrease Junction Deviation to slow more at corners and disable Linear Advance, as further research has confirmed the 4.2.2 board should not be running with it enabled, unless the board gets modified

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1300,7 +1300,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.045 // (mm) Distance from real junction edge (calculated formula d= 0.4 * (Jerk ^2)/Acceleration)(0.043 = 0.4 * 9*9 / 750) Values based on online starting base settings
+  #define JUNCTION_DEVIATION_MM 0.035 // (mm) Distance from real junction edge (calculated formula d= 0.4 * (Jerk ^2)/Acceleration)(0.043 = 0.4 * 9*9 / 750) Values based on online starting base settings
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
